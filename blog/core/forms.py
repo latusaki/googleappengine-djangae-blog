0e311829d44ab5a5263558c07566e0f1ac7fef3b
Ioannis Latousakis <latusaki@gmail.com>
2015-07-28 19:45:00 +0300

    GAE didn't like our previous hidden field approach... just override Form save instead.

--- blog/core/forms.py
+++ blog/core/forms.py
@@ -5,18 +5,26 @@ from blog.core.models import Post, Blog
 from google.appengine.api import users
 
 class PostForm(forms.ModelForm):
-    if users.get_current_user() is not None:
-        user_id = users.get_current_user().user_id()
-        user_email = users.get_current_user().email()
-        author_id = forms.IntegerField(widget=forms.HiddenInput(), initial=user_id)
-        author_email = forms.CharField(widget=forms.HiddenInput(), initial=user_email)
 
     class Meta:
         model = Post
-        fields = ('title', 'body','author_id','author_email')
+        fields = ('title', 'body',)
         labels = {
             'body': ('Content')
         }
+   
+    def save(self, force_insert=False, force_update=False, commit=True):
+        obj = super(PostForm, self).save(commit=False)
+        if users.get_current_user() is not None:
+            user_id = users.get_current_user().user_id()
+            user_email = users.get_current_user().email()    
+            obj.author_id = user_id
+            obj.author_email = user_email
+        if commit:
+            obj.save()
+        
+        return obj
+
 
 
 class BlogForm(forms.ModelForm):
@@ -24,5 +32,5 @@ class BlogForm(forms.ModelForm):
         model = Blog
         fields = ('title', 'tagline','paginate_by')
         labels = {
-            'paginate_by': ('Post per page')
+            'paginate_by': ('Posts per page')
         }
